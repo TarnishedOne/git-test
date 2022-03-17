7d9ace2a6b6c06c7e8be2bd280569af8aceeae13
TarnishedOne <840143158@qq.com>
2022-03-17 15:57:12 +0800

    second commit-20-03-17

--- src/main/java/com/ashen/one/common/GitTest.java
+++ src/main/java/com/ashen/one/common/GitTest.java
@@ -3,5 +3,6 @@ package com.ashen.one.common;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hi,Git1");
+        System.out.println("Hi,Git2");
     }
 }
