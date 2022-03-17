57caa1743c7e284b92a0127665ecf32584b21a25
TarnishedOne <840143158@qq.com>
2022-03-17 15:57:32 +0800

    thirrd commit-20-03-17

--- src/main/java/com/ashen/one/common/GitTest.java
+++ src/main/java/com/ashen/one/common/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hi,Git1");
         System.out.println("Hi,Git2");
+        System.out.println("Hi,Git3");
     }
 }
